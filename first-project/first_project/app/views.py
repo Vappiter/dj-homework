7b6d5358eac8362591091459fd2e1cc4021ebc80
Andrey Vladimirov <vappiter@yandex.ru>
2022-04-08 00:10:35 +0300

    Lesson1 TWO change

--- first-project/first_project/app/views.py
+++ first-project/first_project/app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
-
+from datetime import datetime
+import os
 
 def home_view(request):
     template_name = 'app/home.html'
@@ -8,8 +9,8 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('dir')
     }
     
     # context и параметры render менять не нужно
@@ -23,8 +24,10 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
+    time = datetime.now()
+    current_time = time.strftime("%H:%M:%S")
     msg = f'Текущее время: {current_time}'
+      # , 'Вернуться на главнаю страницу': reverse('home')
     return HttpResponse(msg)
 
 
@@ -32,4 +35,7 @@ def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
     # который возвращает список файлов в рабочей 
     # директории
+    files = os.listdir(".")
+    msg =f'Файлы в текущей директории: {files}'  
+    return HttpResponse(msg)
     raise NotImplemented
--- first-project/lesson1/urls.py
+++ first-project/lesson1/urls.py
@@ -1,7 +1,7 @@
-"""first_project URL Configuration
+"""lesson1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,20 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
-from app.views import home_view
-from app.views import time_view
-from app.views import workdir_view
-
+from django.urls import path
 
 urlpatterns = [
-    path('', home_view, name='home'),
-    path('time/', time_view, name='time'),
-    path('dir/', workdir_view, name='dir'),
-    # Раскомментируйте код, чтобы данные урлы 
-    # обрабатывались Django
-    # path('current_time/', time_view, name='time'),
-    # path('workdir/', workdir_view, name='workdir'),
     path('admin/', admin.site.urls),
 ]
